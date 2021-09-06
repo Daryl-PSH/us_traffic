90d46ac35229c63f3e4b4925a2fc16bd36ab8910
daryl_poon <daryl_poon@kelaberetiv.sg>
2021-09-06 16:27:29 +0800

    fix remove_redundant_column func

--- src/data_pipeline/preprocess_data.py
+++ src/data_pipeline/preprocess_data.py
@@ -43,7 +43,7 @@ def combine_data(traffic_df: pd.DataFrame, station_df: pd.DataFrame) -> pd.DataF
 
     return combined_df
 
-def drop_remapped_column(traffic_df: pd.DataFrame, station_df: pd.DataFrame) -> Tuple(pd.DataFrame):
+def drop_remapped_column(traffic_df: pd.DataFrame, station_df: pd.DataFrame) -> Tuple[pd.DataFrame]:
     """
     Drop remapped columns that have been identified in the Data_Cleaning.ipynb before the state code filter
     of the dataframe
@@ -55,11 +55,11 @@ def drop_remapped_column(traffic_df: pd.DataFrame, station_df: pd.DataFrame) -> 
     Returns:
         Tuple (DataFrame): Tuple containing (traffic_df, station_df)
     """
-    station_columns_to_drop = ["algorithm_of_classification_name", "calibration_of_weighing_system_name",
+    station_columns_to_drop = ["algorithm_of_vehicle_classification_name", "calibration_of_weighing_system_name",
                                "direction_of_travel", "functional_classification", "lane_of_travel_name",
                                "method_of_data_retrieval_name", "method_of_traffic_volume_counting_name",
                                "method_of_truck_weighing", "method_of_vehicle_classification_name",
-                               "sample_type_of_traffic_volume", "sample_type_for_truck_weight_name",
+                               "sample_type_for_traffic_volume", "sample_type_for_truck_weight_name",
                                "sample_type_for_vehicle_classification_name", "type_of_sensor"]
 
     traffic_df.drop(["restrictions"], inplace=True, axis=1)
@@ -68,7 +68,7 @@ def drop_remapped_column(traffic_df: pd.DataFrame, station_df: pd.DataFrame) -> 
     return (traffic_df, station_df)
 
 def filter_by_state_code(traffic_df: pd.DataFrame, station_df: pd.DataFrame,
-                         state_code: int) -> Tuple(pd.DataFrame):
+                         state_code: int) -> Tuple[pd.DataFrame]:
     """
     Filter the dataframe by state code, keeping stations that belong to the specified code
     in the state_code argument.
@@ -99,7 +99,7 @@ def convert_established_year_to_actual_year(traffic_df: pd.DataFrame) -> pd.Data
     Returns:
         pd.DataFrame: DataFrame with year_station_established column
     """
-    traffic_df["year_station_established"].apply(lambda x: 2000 + x if x <= 15 else 1900 + x)
+    traffic_df["year_station_established"] = traffic_df["year_station_established"].apply(lambda x: 2000 + x if x <= 15 else 1900 + x)
 
     return traffic_df
 
@@ -115,12 +115,12 @@ def remove_redundant_column(df: pd.DataFrame) -> pd.DataFrame:
         pd.DataFrame: DataFrame with columns removed
     """
     for column in df.columns:
-        if len(df[column].unique() == 1):
+        if len(df[column].unique()) == 1:
             df.drop(column, inplace=True, axis=1)
 
     return df
 
-def columns_to_drop(columns_to_drop: List[str], df: pd.DataFrame) -> pd.DataFrame:
+def drop_columns(columns_to_drop: List[str], df: pd.DataFrame) -> pd.DataFrame:
     """
     Drop columns specified in columns_to_drop list
 
