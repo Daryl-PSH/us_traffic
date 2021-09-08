6b269d3cffdf75cfb91d2dba22db3d3db30a18ec
daryl_poon <daryl_poon@kelaberetiv.sg>
2021-09-08 22:16:26 +0800

    Remove logging

--- src/data_pipeline/preprocess_data.py
+++ src/data_pipeline/preprocess_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from feature_engineering import *
 
 import datetime as datetime
 from dateutil.relativedelta import relativedelta
@@ -7,6 +8,51 @@ from typing import Tuple, List
 
 RAW_STATION_DATA_PATH = Path("data/raw/dot_traffic_stations_2015.txt.gz")
 RAW_TRAFFIC_DATA_PATH = Path("data/raw/dot_traffic_2015.txt.gz")
+STATE_CODE = 36
+
+def preprocess_data(traffic_data_path: Path, station_data_path: Path) -> pd.DataFrame:
+    """
+    Preprocess the data to be used for encoding, first step of the data pipeline
+
+    Args:
+        traffic_data_path (Path): Filepath to the traffic csv
+        station_data_path (Path): Filepath to the station csv
+
+    Returns:
+        pd.DataFrame:
+    """
+
+ 
+    traffic_df, station_df = load_data(traffic_data_path, station_data_path)
+
+    traffic_df, station_df = drop_remapped_column(traffic_df, station_df)
+    traffic_df, station_df = filter_by_state_code(traffic_df, station_df, state_code=STATE_CODE)
+    station_df = clean_sample_type_for_vehicle_classification_column(station_df)
+
+    # Combine dataframe
+    combined_df = combine_data(traffic_df, station_df)
+
+    # Feature engineering
+    combined_df = convert_established_year_to_actual_year(combined_df)
+    combined_df = create_years_of_operation_column(combined_df)
+    combined_df = create_peak_hour_traffic_volume_column(combined_df, rush_hour_type="pm")
+
+    # Final column drop before encoding features
+    combined_df = remove_redundant_column(combined_df)
+    combined_df = drop_future_volume_information(combined_df, "pm")
+
+    columns_to_drop = ["year_station_established", "station_location",
+                       "previous_station_id", "latitude", "longitude",
+                       "method_of_truck_weighing_name", "fips_county_code",
+                       "direction_of_travel", "station_id", "hpms_sample_identifier",
+                       "algorithm_of_vehicle_classification", "functional_classification_name"]
+
+    combined_df = drop_columns(columns_to_drop, combined_df)
+    combined_df = drop_na_columns(combined_df)
+
+    combined_df.to_csv("data/interim/cleaned_data.csv", index=False)
+
+    return combined_df
 
 def load_data(traffic_data_path: Path, station_data_path: Path) -> Tuple[pd.DataFrame]:
     """
