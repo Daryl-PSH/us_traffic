1afa0111358d2b9ef1294e84f5b56b1735cfae9b
daryl_poon <daryl_poon@kelaberetiv.sg>
2021-09-11 11:45:12 +0800

    Remove unecessary __init__

--- src/models/model_factory.py
+++ src/models/model_factory.py
@@ -5,9 +5,6 @@ from src.models.model import *
 
 class ModelFactory:
 
-    def __init__(self):
-        self._load_conf()
-
     def create_model(self, model_type: str, params: Dict[str, Union[int, str]]):
         if model_type == "random_forest":
             return RandomForest(params)
